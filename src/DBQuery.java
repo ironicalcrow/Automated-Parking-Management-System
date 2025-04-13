4472fbff820141f1246d0071c18014373902258c
ironicalcrow <arhamkhan@iut-dhaka.edu>
2025-04-13 20:56:43 +0600

    last er tar naam  bhule first push disilam

--- src/DBQuery.java
+++ src/DBQuery.java
@@ -3,7 +3,17 @@ import java.sql.*;
 
 public class DBQuery {
     Connection conn = DBConnection.getConnection();
-
+    private static DBQuery instance;
+    private DBQuery() throws SQLException {
+        conn = DBConnection.getConnection();
+    }
+    
+    public static synchronized DBQuery getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DBQuery();
+        }
+        return instance;
+    }
     public ResultSet executeSelect(String query, Object... params) throws SQLException {
 
         PreparedStatement stmt = conn.prepareStatement(query);
