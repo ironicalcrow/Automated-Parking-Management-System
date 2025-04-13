d75967f7e5294698700784d151bddeb6f5801eb0
ironicalcrow <arhamkhan@iut-dhaka.edu>
2025-04-13 20:48:56 +0600

    First Push

---
+++ src/DBQuery.java
@@ -0,0 +1,33 @@
+
+import java.sql.*;
+
+public class DBQuery {
+    Connection conn = DBConnection.getConnection();
+
+    public ResultSet executeSelect(String query, Object... params) throws SQLException {
+
+        PreparedStatement stmt = conn.prepareStatement(query);
+        setParameters(stmt, params);
+        return stmt.executeQuery();
+    }
+
+    public int executeUpdate(String query, Object... params) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement(query);
+        setParameters(stmt, params);
+        return stmt.executeUpdate();
+    }
+
+    public boolean execute(String query, Object... params) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement(query);
+        setParameters(stmt, params);
+        return stmt.execute();
+    }
+
+    private void setParameters(PreparedStatement stmt, Object... params) throws SQLException {
+        if (params != null) {
+            for (int i = 0; i < params.length; i++) {
+                stmt.setObject(i + 1, params[i]);
+            }
+        }
+    }
+}
